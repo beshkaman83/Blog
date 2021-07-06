49283d3cfbc0d991a9de1a46630a995a2abc10b2
aibek <beshkaman83@gmail.com>
2021-07-06 22:05:45 +0600

    set MEDIA_ROOT, Change model Post, configurate admin

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from post.models import Category, Post, Tag
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'author', 'views', 'get_photo')
+    prepopulated_fields = {'slug': ('title',)}
+    fields = ('title', 'slug', 'author', 'category', 'content', 'tags', 'views', 'photo', 'get_photo')
+    list_filter = ('author', 'category', 'created_at')
+    readonly_fields = ('views','get_photo')
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="50">')
+        return '-'
+
+    get_photo.short_description = 'Фото'
+    
+
